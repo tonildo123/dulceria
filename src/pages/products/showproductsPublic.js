e65dc7f793b01add855b7ec990164ff18941e6cf
tonildo123 <tony81191@gmail.com>
2023-12-06 11:08:54 -0300

    details ok

--- src/pages/products/showproductsPublic.js
+++ src/pages/products/showproductsPublic.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import { useEffect, useState } from 'react';
-import { Typography, Grid, Card, CardContent, CardMedia, Box } from '@mui/material';
+import { Typography, Grid, Card, CardContent, CardMedia, Box, Button } from '@mui/material';
 import { collection, getDocs, } from 'firebase/firestore';
 import { db } from '../../firebase';
 import WelcomeComponent from '../../components/welcomeComponent';
 import { useSelector, useDispatch } from 'react-redux';
 import { productArrayClean, productArraySuccess } from '../../state/ArrayProductSlice';
 import useOffline from '../../hooks/useOffline';
-
+import { useNavigate } from "react-router-dom";
 
 const ShowproductsPublic = () => {
 
@@ -17,6 +17,7 @@ const ShowproductsPublic = () => {
   const dispatch = useDispatch();
   const state = useSelector(state => state)
   const { CargarProductosOffline } = useOffline()
+  const navigate = useNavigate();
 
   const getPets = async () => {
 
@@ -88,6 +89,7 @@ const ShowproductsPublic = () => {
   const renderCard = (card, index) => {
     return (
       <Grid item key={card.id} sx={{ width: { sx: '100%', sm: '200px', height: { sx: '200px', sm: '300px' } } }}>
+        <Button onClick={() => navigate(`/detalles`, { state: { card } })}>
         <Card sx={{ my: '2px' }} >
           <CardMedia
             component="img"
@@ -102,6 +104,7 @@ const ShowproductsPublic = () => {
             <Typography variant="body2" sx={{ whiteSpace: 'nowrap', fontWeight: 'bold' }}>{card.stock} unidades</Typography>
           </CardContent>
         </Card>
+        </Button>
       </Grid>
     );
   };
